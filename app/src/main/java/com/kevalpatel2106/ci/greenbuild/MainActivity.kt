7ec9ddb99eb3a8a14a04905896e823959aaf2040
Keval Patel <kevalpatel2106@gmail.com>
2018-04-15 19:00:42 +0530

    Initilizing the repo

---
+++ app/src/main/java/com/kevalpatel2106/ci/greenbuild/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kevalpatel2106.ci.greenbuild
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
