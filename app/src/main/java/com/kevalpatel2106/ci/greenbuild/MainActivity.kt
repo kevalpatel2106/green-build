7d9dc3d29e1a0fd7439181f897b60204f25d9ae4
Keval Patel <kevalpatel2106@gmail.com>
2018-04-16 08:52:59 +0530

    Setting up build dependencies

--- app/src/main/java/com/kevalpatel2106/ci/greenbuild/MainActivity.kt
+++ app/src/main/java/com/kevalpatel2106/ci/greenbuild/MainActivity.kt
@@ -1,8 +1,19 @@
+/*
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package com.kevalpatel2106.ci.greenbuild
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
+/**
+ * Main activity.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
