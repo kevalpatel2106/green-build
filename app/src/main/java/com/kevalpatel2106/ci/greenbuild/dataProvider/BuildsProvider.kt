0ef4f90ea615adc7866f0b71fe8ef0f0d38a5a4b
Keval Patel <kevalpatel2106@gmail.com>
2018-05-11 10:28:33 +0530

    Adding build status badge for travis-ci-.com (#12)
    Adding repo provider to load repo list using repository

--- app/src/main/java/com/kevalpatel2106/ci/greenbuild/dataProvider/BuildsProvider.kt
+++ app/src/main/java/com/kevalpatel2106/ci/greenbuild/dataProvider/BuildsProvider.kt
@@ -14,19 +14,18 @@
 
 package com.kevalpatel2106.ci.greenbuild.dataProvider
 
-import com.kevalpatel2106.ci.greenbuild.base.ciInterface.CompatibilityCheck
-import com.kevalpatel2106.ci.greenbuild.base.ciInterface.Page
-import com.kevalpatel2106.ci.greenbuild.base.ciInterface.ServerInterface
+import com.kevalpatel2106.ci.greenbuild.base.ciInterface.*
 import com.kevalpatel2106.grrenbuild.entities.Build
-import com.kevalpatel2106.ci.greenbuild.base.ciInterface.BuildSortBy
 import com.kevalpatel2106.grrenbuild.entities.BuildState
+import com.kevalpatel2106.grrenbuild.entities.Repo
 import io.reactivex.Observable
 import javax.inject.Inject
 
 /**
  * Created by Kevalpatel2106 on 04-May-18.
- * A class that provides the data to the view models and acts as a repository. This class will
- * responsible for connecting with CI platform servers and database.
+ * A class that provides the data for the [Build] to the view models and acts as a repository. This
+ * class will responsible for connecting with CI platform servers and database to perform [Build]
+ * operations.
  *
  * @constructor Dagger injectable public constructor.
  * @param serverInterface [ServerInterface] for making the api calls to the CI servers.
@@ -34,7 +33,7 @@ import javax.inject.Inject
  * @param GBRxSchedulers [GBRxSchedulers] to provide threads.
  * @author <a href="https://github.com/kevalpatel2106">kevalpatel2106</a>
  */
-internal class DataProvider @Inject constructor(
+internal class BuildsProvider @Inject constructor(
         private val serverInterface: ServerInterface,
         private val compatibilityCheck: CompatibilityCheck,
         private val GBRxSchedulers: GBRxSchedulers
@@ -52,7 +51,45 @@ internal class DataProvider @Inject constructor(
             repoSortBy: BuildSortBy,
             buildState: BuildState? = null
     ): Observable<Page<Build>> {
+        if (!compatibilityCheck.isRecentBuildsListSupported())
+            throw IllegalStateException("Recent builds listing by repository is not supported for this CI.")
+
         return serverInterface.getRecentBuildsList(page, repoSortBy, buildState)
+                .observeOn(GBRxSchedulers.main)
+                .subscribeOn(GBRxSchedulers.network)
+    }
+
+    /**
+     * Restart the [build]. You can only restart the build which has status other than
+     * [BuildState.RUNNING] or [BuildState.BOOTING].
+     *
+     * @return It will return the [Observable] with the id of the new build.
+     */
+    fun restartBuild(build: Build): Observable<String>{
+        if (!compatibilityCheck.isRestartBuildSupported())
+            throw IllegalStateException("Restart build is not supported for this CI platform.")
+
+        if (build.state == BuildState.RUNNING || build.state == BuildState.BOOTING)
+            throw IllegalStateException("Cannot restart running or booting build")
+
+        return serverInterface.restartBuild(build)
+                .observeOn(GBRxSchedulers.main)
+                .subscribeOn(GBRxSchedulers.network)
+    }
+
+    /**
+     * Abort the [build]. You can abort the build that are in [BuildState.RUNNING] only.
+     *
+     * @return It will return the [Observable] with the id of the new build.
+     */
+    fun abortBuild(build: Build): Observable<String>{
+        if (!compatibilityCheck.isAbortBuildSupported())
+            throw IllegalStateException("Abort build is not supported for this CI platform.")
+
+        if (build.state != BuildState.RUNNING)
+            throw IllegalStateException("Can only abort running build.")
+
+        return serverInterface.abortBuild(build)
                 .observeOn(GBRxSchedulers.main)
                 .subscribeOn(GBRxSchedulers.network)
     }
